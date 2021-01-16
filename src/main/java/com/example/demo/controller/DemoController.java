8266f412d4311fc2625dd9eed654be8c97b668b9
Mohammad Samim Akhtar <mohammad.samimakhtar1@t-mobile.com>
2021-01-16 12:50:56 +0530

    egher

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -3,7 +3,7 @@ package com.example.demo.controller;
 public class DemoController {
 	
 	public static void main(String[] args) {
-		System.out.println("welcome hello world");
+		System.out.println("welcome world");
 		System.out.println("Hi");
 	}
 
